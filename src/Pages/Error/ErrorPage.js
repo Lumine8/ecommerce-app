15721adf87b1e0c3875f843f591f10f05d2a55f9
Lumine8 <sankargopan1@gmail.com>
2023-07-19 01:47:14 +0530

    Update ErrorPage.js

--- src/Pages/Error/ErrorPage.js
+++ src/Pages/Error/ErrorPage.js
@@ -1,11 +1,13 @@
 import "./Error.css"
 import {TbNumber5,TbNumber0} from "react-icons/tb"
-import {CiFaceFrown} from "react-icons/ci"
+import {FaRegFaceDizzy, } from "react-icons/fa6"
+
 export default function ErrorPage(){
     return(
         <div id="errorPage">
             <h1><TbNumber5/> <TbNumber0/> <TbNumber5/></h1>
-            <h2>Something Went Wrong <CiFaceFrown size={50} style={{marginBottom:"-1rem"}}/> </h2>
+            <h2>Something Went Wrong <FaRegFaceDizzy size={50} style={{marginBottom:"-1rem"}}/> </h2>
+            <p>Try Signing in, maybe.</p>
         </div>
     )
 }
